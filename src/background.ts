63ca3daf09354ca1bf1362d04a1902ddf453d110
liguorui <136320309@qq.com>
2024-05-21 18:31:31 +0800

    侧边栏

---
+++ src/background.ts
@@ -0,0 +1,8 @@
+console.log("Hello, world!");
+
+chrome.action.onClicked.addListener(() => {
+  console.log(`action clicked: `)
+})
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
